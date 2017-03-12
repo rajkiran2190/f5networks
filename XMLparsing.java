0efe53a9c0f416eb1e70eb1a07a92de77126fad6
Rajkiran <rajkiran2190@gmail.com>
2017-03-12 16:12:12 -0700

    with post request

--- XMLparsing.java
+++ XMLparsing.java
@@ -4,7 +4,14 @@ package f5networks;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+import java.nio.file.Files;
 import java.util.Base64;
 
 
@@ -20,6 +27,7 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Node;
+
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
@@ -29,7 +37,7 @@ import org.xml.sax.SAXException;
 
 public class XMLparsing {
 	private final static String filename = "outputxml";
-
+	private final static String url = "http://posttestserver.com/post.php";
 	public static void main(String[] args) {
 		for(int i = 0; i < args.length; i++) {
             System.out.println("rajkiran " + args[i]);
@@ -62,7 +70,35 @@ public class XMLparsing {
 			Transformer transformer = transformerFactory.newTransformer();
 			DOMSource source = new DOMSource(doc);
 			StreamResult result = new StreamResult(new File(filename));
-			transformer.transform(source, result);			
+			transformer.transform(source, result);		
+			
+			
+			String charset = "UTF-8";
+			String param = "value";
+			File textFile = new File(filename);
+
+			String boundary = Long.toHexString(System.currentTimeMillis());
+			String CRLF = "\r\n";
+			URLConnection connection = new URL(url).openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+
+			OutputStream output = connection.getOutputStream();
+			PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
+
+			writer.append("--" + boundary).append(CRLF);
+			writer.append("Content-Disposition: form-data; name=\"textFile\"; filename=\"" + textFile.getName() + "\"").append(CRLF);
+			writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF); // Text file itself must be saved in this charset!
+			writer.append(CRLF).flush();
+			Files.copy(textFile.toPath(), output);
+			output.flush(); // Important before continuing with writer!
+			writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+			// Request is lazily fired whenever you need to obtain information about response.
+			int responseCode = ((HttpURLConnection) connection).getResponseCode();
+			System.out.println(responseCode); // Should be 200
+
+
 				
 		} catch (IOException e) {			
 			e.printStackTrace();
